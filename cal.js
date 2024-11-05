694ba09d984dc2e8381c2a88d3c77df1dfb85afb
Minuku.SunilKumar <154435816+minukusunilkumar1910@users.noreply.github.com>
2024-11-05 10:03:25 +0530

    my project on calculator

---
+++ cal.js
@@ -0,0 +1,13 @@
+// prompt("hello");
+// let del =document.getElementById('del')
+// let display=document.getElementById('display')
+// let equal=document.getElementsById('equal')
+
+// del.addEventListener("click",()=>
+// {
+//    display.value = display.value.toString().slice(0,-1);
+// })
+
+ 
+
+
